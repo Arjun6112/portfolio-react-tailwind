914003f58010a15eac97cfa6e6b9ba3d96378f6b
Arjun <6112arjun@gmail.com>
2023-07-06 20:53:57 +0530

    Header Done

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,90 @@
-import React from 'react';
+import React from "react";
+import Image from "../assets/avatar.svg";
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
 
 const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <section
+      id="home"
+      className="min-h-[85vh] lg:min-h-[78vh] flex items-center"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12">
+          <div className="flex-1 text-center font-secondary lg:text-left  ">
+            <motion.h1
+              variants={fadeIn("up", 0.5)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="text-[55px] font-bold leading-[0.8] lg:text-[110px]"
+            >
+              Ar<span>jun</span>
+            </motion.h1>
+            <motion.div
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mt-6 mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]"
+            >
+              <span className="text-white mr-4 ">I am a</span>
+              <TypeAnimation
+                sequence={["Developer", 2000, "Freelancer", 2000, "Musician"]}
+                speed={50}
+                className="text-accent"
+                wrapper="span"
+                repeat={Infinity}
+              />
+            </motion.div>
+            <motion.p
+              variants={fadeIn("up", 0.5)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mb-8 max-w-lg mx-auto lg:mx-0"
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex odio
+              excepturi reiciendis beatae facere blanditiis!
+            </motion.p>
+            <motion.div
+              variants={fadeIn("up", 0.6)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0"
+            >
+              <button className="btn btn-lg">Contact</button>
+              <a href="#" className="text-gradient btn-link">
+                My Portfolio
+              </a>
+            </motion.div>
+            <motion.div
+              variants={fadeIn("up", 0.7)}
+              initial="hidden"
+              whileInView={"show"}
+              className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0"
+            >
+              <a href="#">
+                <FaGithub />
+              </a>
+              <a href="#">
+                <FaLinkedin />
+              </a>
+              <a href="#">
+                <FaInstagram />
+              </a>
+            </motion.div>
+          </div>
+          <div className="hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px]">
+            <img src={Image} alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Banner;
